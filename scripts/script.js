a0ebc6d76e43a45151ec7d24a9b38eec5a5cee22
supminn <30731236+supminn@users.noreply.github.com>
2021-01-20 21:26:00 +0530

    style: mobile design

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,10 @@
+let content = document.querySelectorAll(".faq-content");
 let questions = document.querySelectorAll(".faq-question");
 let answer = document.querySelectorAll(".faq-answer");
 let arrow = document.querySelectorAll(".img-arrow");
 
-for(let i=0;i<questions.length;i++){
-    questions[i].addEventListener('click',() => {
+for(let i=0;i<content.length;i++){
+    content[i].addEventListener('click',() => {
         questions[i].classList.toggle('faq-question-active');
         answer[i].classList.toggle('hidden');
         arrow[i].classList.toggle('img-arrow-active');
